28a0805884644a65118dd9a91a5a51dc0461d8d3
dylee-spec <dy.lee@udm.kr>
2026-09-25 13:30:22 +0900

    Update gtrends_top.sql

--- scripts/weeklip/gtrends_top.sql
+++ scripts/weeklip/gtrends_top.sql
@@ -1,4 +1,3 @@
--- 한국 구글 인기 검색어 (가장 최근 집계일, 최신 주 기준)
 WITH latest AS (
   SELECT MAX(refresh_date) AS d FROM `bigquery-public-data.google_trends.international_top_terms` WHERE country_code = 'KR'
 )
