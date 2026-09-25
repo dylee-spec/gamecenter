487c38f67805b4eb3420735723792b1c97d25d25
dylee-spec <dy.lee@udm.kr>
2026-09-25 13:30:03 +0900

    Update gtrends_rising.sql

--- scripts/weeklip/gtrends_rising.sql
+++ scripts/weeklip/gtrends_rising.sql
@@ -1,4 +1,3 @@
--- 한국 구글 급상승 검색어 (가장 최근 집계일, 최신 주 기준)
 WITH latest AS (
   SELECT MAX(refresh_date) AS d FROM `bigquery-public-data.google_trends.international_top_rising_terms` WHERE country_code = 'KR'
 )
