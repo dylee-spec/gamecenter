d16436bf383ae6eb8d3591615ed2eaa03299477f
dylee-spec <dy.lee@udm.kr>
2026-09-25 09:09:03 +0900

    Create gtrends_rising.sql

---
+++ scripts/weeklip/gtrends_rising.sql
@@ -0,0 +1,10 @@
+-- 한국 구글 급상승 검색어 (가장 최근 집계일, 최신 주 기준)
+WITH latest AS (
+  SELECT MAX(refresh_date) AS d FROM `bigquery-public-data.google_trends.international_top_rising_terms` WHERE country_code = 'KR'
+)
+SELECT term, MIN(rank) AS rank, MAX(percent_gain) AS percent_gain, ANY_VALUE(refresh_date) AS refresh_date, MAX(week) AS week
+FROM `bigquery-public-data.google_trends.international_top_rising_terms`, latest
+WHERE country_code = 'KR' AND refresh_date = latest.d
+  AND week = (SELECT MAX(week) FROM `bigquery-public-data.google_trends.international_top_rising_terms`, latest WHERE country_code = 'KR' AND refresh_date = latest.d)
+GROUP BY term
+ORDER BY rank
